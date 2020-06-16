122472b8b758c267b54e31ce34a067424afe1eaa
dennisn00 <dxnnxs@protonmail.com>
2020-06-16 15:58:31 +0200

    added test screen

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/test/TestFragment.kt
@@ -0,0 +1,57 @@
+package de.rki.coronawarnapp.ui.test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityEvent
+import de.rki.coronawarnapp.databinding.FragmentInformationAboutBinding
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.ui.main.MainActivity
+
+/**
+ * Basic Fragment which only displays static content.
+ */
+class TestFragment : Fragment() {
+    companion object {
+        private val TAG: String? = TestFragment::class.simpleName
+    }
+
+    private var _binding: FragmentInformationAboutBinding? = null
+    private val binding: FragmentInformationAboutBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInformationAboutBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setButtonOnClickListener()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.informationAboutScrollview.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.informationAboutScrollview.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED)
+    }
+
+    private fun setButtonOnClickListener() {
+        binding.informationAboutHeader.headerButtonBack.buttonIcon.setOnClickListener {
+            (activity as MainActivity).goBack()
+        }
+    }
+}
